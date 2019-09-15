a6a0e41d637ac5361f0c6f10552c26f0607212fa
anandhu-dev <54721685+anandhu-dev@users.noreply.github.com>
2019-09-16 00:48:26 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -20,8 +20,8 @@ from tg_bot.modules.helper_funcs.misc import paginate_modules
 PM_START_TEXT = """
 ..........................................👇🏻
 
-*Hey {}, My name is {}! 
-im a Group Manager bot Maintained by*  [This Person](tg://user?id={}).
+Hey *{}*, My name is *{}!* 
+im a Group Manager bot Maintained by  [This Person](tg://user?id={}).
 
 *Hit /help to Find Out More About How to Use Me.*😊
 
